53764d45e36da4c3848f1b548b0cd0b26080ac05
soleilsolis <fukuman23@gmail.com>
2023-01-18 07:38:43 +0800

    asdf

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
-            {{ __('Product Types') }}
+            {{ __('Products') }}
         </h2>
 
         <div class="mt-4">
